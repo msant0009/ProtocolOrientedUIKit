ebb4afa6faea0e1750713ab44b178ac147473017
Mark Santoro <mark_santoro@comcast.net>
2024-09-11 15:49:37 -0700

    add labels programmaticaly

--- ProtocolOrientedUIKit/UserViewController.swift
+++ ProtocolOrientedUIKit/UserViewController.swift
@@ -7,16 +7,73 @@
 
 import UIKit
 
+private let nameLabel : UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.textAlignment = .center
+    return label
+}()
+
+private let emailLabel : UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.textAlignment = .center
+    return label
+}()
+
+private let usernameLabel : UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.textAlignment = .center
+    return label
+}()
+
+
 class UserViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .yellow
+        setupViews()
         
         
     }
 
+    
+    func setupViews() {
+        view.backgroundColor = .yellow
+        
+        view.addSubview(emailLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(usernameLabel)
+        
+        NSLayoutConstraint.activate(
+        [
+            nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nameLabel.heightAnchor.constraint(equalToConstant: 60),
+            nameLabel.widthAnchor.constraint(equalToConstant: 200),
+            nameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),// 100 from the top
+            
+            emailLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emailLabel.heightAnchor.constraint(equalToConstant: 60),
+            emailLabel.widthAnchor.constraint(equalToConstant: 200),
+            emailLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10),// 10 from the bottom of name
+            
+            usernameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 60),
+            usernameLabel.widthAnchor.constraint(equalToConstant: 200),
+            usernameLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 10)// 10 from the bottom of name
+            
+            
+        
+        ])
+        
+        nameLabel.text = "Name"
+        emailLabel.text = "email"
+        usernameLabel.text = "UserName"
+        
+        
+    }
 
 }
 
